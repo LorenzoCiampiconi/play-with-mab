63b15e9b0fbf2048a4ae7f06279f489fdc3a2073
lciampiconi <lorenzo.ciampiconi@mail.polimit.it>
2022-11-11 10:34:46 +0100

    updated requirements.txt main expliciting functioning

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from distribution import GaussianDistribution
 from mab import MabProblem
-g_1 = GaussianDistribution(0,1)
-g_2 = GaussianDistribution(0.5,2)
-g_3 = GaussianDistribution(4,10)
-g_4 = GaussianDistribution(3,5)
+g_1 = GaussianDistribution(0, 1)
+g_2 = GaussianDistribution(0.5, 2)
+g_3 = GaussianDistribution(4, 10)
+g_4 = GaussianDistribution(3, 5)
 mab_problem = MabProblem({1: g_1, 2: g_2, 3: g_3, 4: g_4})
