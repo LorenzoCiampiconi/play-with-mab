718566f5e49b017e188560840db8c1253fcb99b6
lciampiconi <lorenzo.ciampiconi@mail.polimi.it>
2022-12-06 11:25:10 +0100

    gui modules refactoring

--- game_core/main_objectified.py
+++
@@ -1,5 +0,0 @@
-from game_core.gui.bmab_gui import BarcelonaMabGUI
-
-if __name__ == "__main__":
-    gui = BarcelonaMabGUI()
-    gui.play_window_process()
--- game_core/main_simulation.py
+++
@@ -1,7 +0,0 @@
-from game_core.gui.simulating_gui import BarcelonaMABAlgorithmSimulatingGUI
-from game_core.simulation.algorithm import RandomAlgorithm, UpperConfidenceBound1, ThompsonSampling
-
-if __name__ == "__main__":
-    algorithm_class = ThompsonSampling
-    gui = BarcelonaMABAlgorithmSimulatingGUI(simulate=False, algorithm_class=algorithm_class, algorithm_kwargs={})
-    gui.menu_window_process()
